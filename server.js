456c6e2396d1ac45de4422dace95b4b192ec7e20
angelaxie2015 <angelaxie2015@gmail.com>
2021-03-15 19:40:02 -0400

    set up mongodb

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-// Specification for http server
-var http = require('http'), 
-	fs = require('fs'), 
- 	url = require('url'),
-  	port = 8080;
+import express from "express"
+import mongoose from "mongoose"
+
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = "mongodb+srv://admin:@cluster0.lb7ej.mongodb.net/ufia-db?retryWrites=true&w=majority"
+
+
+//Database configuration 
+mongoose.connect(connection_url, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.listen(port, () => console.log('Listening on port ${port}'));
+
+
+
+// // Specification for http server
+// var http = require('http'), 
+// 	fs = require('fs'), 
+//  	url = require('url'),
+//   	port = 8080;
