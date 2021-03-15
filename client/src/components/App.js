94df371cb4ec03eef53cca7dbc2c7777caa6e7ac
angelaxie2015 <angelaxie2015@gmail.com>
2021-03-15 00:06:40 -0400

    install react-dom, route log-in page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import React from 'react';
+import Login from './login/login.js'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+
+
+
+
+    <BrowserRouter>
+        <Login />
+        <Switch>
+          <Route path='/login' component={Login}/>
+
+          <Route path='/'>
+            <Redirect to='/' />
+          </Route>
+        </Switch>
+    </BrowserRouter>
 
     </div>
   );
---
+++ client/src/components/login/login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './login.css'
+
+
+function Login() {
+
+  return (
+    <h1>Hi</h1>
+
+  )
+}
+
+export default Login;
