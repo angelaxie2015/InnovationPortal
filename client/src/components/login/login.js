d54a9afc061d54785de55d32583eab8b2410de6d
angelaxie2015 <angelaxie2015@gmail.com>
2021-03-15 00:28:21 -0400

    add image and form

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,13 +1,48 @@
 import React, { useState } from 'react'
 import './login.css'
 
+class Login extends React.Component {
 
-function Login() {
+	constructor(props) {
+	    super(props);
+	    this.state = {value: ''};
+	    this.handleChange = this.handleChange.bind(this);
+	    this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-  return (
-    <h1>Hi</h1>
+	handleChange(event) {
+	    this.setState({value: event.target.value});
+    }
 
-  )
+	handleSubmit(event) {
+	    alert('A name was submitted: ' + this.state.value);
+	    event.preventDefault();
+	}
+
+	render() {
+	  return (
+	  	<div id="log-in">
+	  		<div>
+	  			<img class="login-image" src="../../ia_logo.png"></img>
+	    		<h1>Login</h1>
+
+		    	<div>
+		    		Hello
+		    	</div>
+
+
+		    	 <form onSubmit={this.handleSubmit}>
+	        		<label> Name:
+	          			<input type="text" value={this.state.value} onChange={this.handleChange} />
+	        		</label>
+	        		<input type="submit" value="Submit" />
+	        	</form>
+
+	    	</div>
+	    </div>
+
+	  )
+	}
 }
 
 export default Login;
