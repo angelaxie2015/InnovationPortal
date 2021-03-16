290463e160b3415ef323a0f2938287f75ec84b0c
angelaxie2015 <angelaxie2015@gmail.com>
2021-03-15 22:50:45 -0400

    hash password and add to database

--- userRouter.js
+++ userRouter.js
@@ -1,19 +1,21 @@
 import express from "express"
+import bcrypt from "bcryptjs"
+import User from "./userdb.js"
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
 	try{
-		const {email, password, checkPassword, userName} = req.body;
+		const {email, pass, checkPassword, userName} = req.body;
 
 		//validating all fields
-		if(!email || !password || !checkPassword || !userName){
+		if(!email || !pass || !checkPassword || !userName){
 			return res
 				.status(400)
 				.json({ msg: "Please enter all fields."});
 		}
 		//check if the user is unique
-		const existUser = User.findOne({email});
+		const existUser = await User.findOne({email});
 		if(existUser){
 			return res
 				.status(400)
@@ -21,20 +23,29 @@ router.post("/register", (req, res) => {
 		}
 
 		//making sure password is at least 8 characters
-		if(password.length < 8){
+		if(pass.length < 8){
 			return res
 				.status(400)
 				.json({ msg: "Password needs to contain at least 8 characters."});
 		}
 
 		//password need to match confirmation password
-		if(password !== checkPassword){
+		if(pass !== checkPassword){
 			return res
 				.status(400)
 				.json({ msg: "Passwords need to match."});
 		}
 
+		//secure password
+		const salt = await bcrypt.genSalt();
+		const password = await bcrypt.hash(pass, salt);
+
+		//store to db
+		const newUser = new User({
+			email, password, userName
+		});
 		
+		const saveUser = await newUser.save();
 
 
 	}catch(err){
