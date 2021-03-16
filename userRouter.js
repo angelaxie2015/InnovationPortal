9bc11f3d171771bc7a9385c87596129f30b2a165
angelaxie2015 <angelaxie2015@gmail.com>
2021-03-15 22:28:54 -0400

    set up userdb router to accept password, username, etc for registration

--- userRouter.js
+++ userRouter.js
@@ -6,9 +6,30 @@ router.post("/register", (req, res) => {
 	try{
 		const {email, password, checkPassword, userName} = req.body;
 
-		console.log(email);
+		//validating all fields
+		if(!email || !password || !checkPassword || !userName){
+			return res
+				.status(400)
+				.json({ msg: "Please enter all fields."});
+		}
+
+		//making sure password is at least 8 characters
+		if(password.length < 8){
+			return res
+				.status(400)
+				.json({ msg: "Password needs to contain at least 8 characters."});
+		}
+
+		//password need to match confirmation password
+		if(password !== checkPassword){
+			return res
+				.status(400)
+				.json({ msg: "Passwords need to match."});
+		}
+
 	}catch(err){
-		console.log("error was here");
+		console.log(err);
+		res.status(500).send();
 	}
 
 	// if(!email || !password || !checkPassword || !userName){
