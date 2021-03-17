6c094f6aecf13f1d2e44b49f36c3fc4a23372cea
kevintram <thekevintram@gmail.com>
2021-03-17 08:56:30 -0400

    Fixes ui issue when there are few events in the event gallery

--- client/src/components/events/event/Event.js
+++ client/src/components/events/event/Event.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     root: {
-      minWidth: "33%",
+      width: 350,
       height: "100%"
     },
     img: {
