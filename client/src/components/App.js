2089f672e4ccd90d40b151b4cff3a6e139b0d815
dev-lukian <devlukian@gmail.com>
2021-03-20 22:08:19 -0400

    wierd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Events from './events/Events'
+import EventDetails from './events/EventDetails';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
 
+=======
+      <Events />
+>>>>>>> events
     </div>
   );
 }
