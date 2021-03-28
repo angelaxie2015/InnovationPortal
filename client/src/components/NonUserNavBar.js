cf41ec3e6ed1069e368a68de5617cdbca701a5e9
angelaxie2015 <angelaxie2015@gmail.com>
2021-03-28 16:39:32 -0400

    redirect to homepage after logging out

--- client/src/components/NonUserNavBar.js
+++ client/src/components/NonUserNavBar.js
@@ -38,6 +38,7 @@ export default function NonUserNavBar() {
             role: undefined,
         });
         localStorage.setItem("auth-token", undefined);
+        history.push("/");
     };
 
     return (
