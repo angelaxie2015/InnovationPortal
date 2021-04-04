6275333267075142bf87756a24b17ebebba93e9e
kevintram <thekevintram@gmail.com>
2021-04-04 13:53:17 -0400

    Adds log out button

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -8,20 +8,32 @@ export default function Dashboard(props) {
     const { user, setUser } = useContext(UserContext);
 
     const history = useHistory();
+
     const addEvent = () => history.push("/addEvent");
+    const logOut = () => {
+        setUser({ token: undefined, user: undefined});
+        history.push("/events")
+    };
 
     if (user.user) {
         return (
             <Grid
                 container
-                spacing={0}
+                spacing={2}
                 direction="column"
                 alignItems="center"
                 justify="center"
                 style={{ minHeight: '100vh' }}
             >
-                <Grid item xs={3}>
-                    <Button onClick={addEvent} >Add Event</Button>
+                <Grid item xs={12}>
+                    <Button onClick={addEvent} variant="contained">
+                        Add Event
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button onClick={logOut} variant="contained">
+                        Log Out
+                    </Button>
                 </Grid>
             </Grid> 
         );
