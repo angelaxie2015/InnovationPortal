b25780be372dc3457513bd58c65b5e0d7d055737
Kullmann <seankullmann@gmail.com>
2021-04-04 18:19:42 -0400

    Created faq page and added link to home page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,26 +1,27 @@
-import './App.css';
+import "./App.css";
 //import EventDetails from './events/EventDetails';
-import React, { useState, useEffect } from 'react';
-import Login from './login/login.js'
-import Register from './login/register.js'
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
-import UserContext from "./context/userContext.js"
-import Axios from "axios"
-import Events from './events/Events';
-import MyForm from './contact/contact.js'
-import NonUserNavBar from "./NonUserNavBar.js"
-import FullScreenEvent from "./events/event/FullScreenEvent.js"
+import React, { useState, useEffect } from "react";
+import Login from "./login/login.js";
+import Register from "./login/register.js";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import UserContext from "./context/userContext.js";
+import Axios from "axios";
+import Events from "./events/Events";
+import MyForm from "./contact/contact.js";
+import NonUserNavBar from "./NonUserNavBar.js";
+import FullScreenEvent from "./events/event/FullScreenEvent.js";
+import faq from "./faqpage/faqpage.js";
 
 function App() {
   const [user, setUser] = useState({
     token: undefined,
-    user: undefined
+    user: undefined,
   });
 
   useEffect(() => {
     const checkLoggedIn = async () => {
       let token = localStorage.getItem("auth-token");
-      if(token === null){
+      if (token === null) {
         localStorage.setItem("auth-token", "");
         token = "";
       }
@@ -28,43 +29,42 @@ function App() {
       const tokenRes = await Axios.post(
         "http://localhost:8001/users/checkToken",
         null,
-        { headers: { "x-auth-token": token} }
+        { headers: { "x-auth-token": token } }
       );
 
-      console.log(tokenRes.data)
-      if(tokenRes.data){
-        const userRes = await Axios.get(
-          "http://localhost:8001/users/",
-          { headers: { "x-auth-token": token} }
-        )
+      console.log(tokenRes.data);
+      if (tokenRes.data) {
+        const userRes = await Axios.get("http://localhost:8001/users/", {
+          headers: { "x-auth-token": token },
+        });
         setUser({
-          token, 
-          user: userRes.data
-        })
+          token,
+          user: userRes.data,
+        });
       }
-
     };
 
     checkLoggedIn();
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <BrowserRouter>
-          <UserContext.Provider value={{user, setUser}}>
-            <NonUserNavBar />
-            <Switch>
-              <Route path='/login' component={Login} />
-              <Route path='/register' component={Register} />
-              <Route path='/contact' component={MyForm} />
-              <Route path='/eventDetail' component={FullScreenEvent} />
-              <Route path='/' component={Events} />
+        <UserContext.Provider value={{ user, setUser }}>
+          <NonUserNavBar />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/contact" component={MyForm} />
+            <Route path="/eventDetail" component={FullScreenEvent} />
+            <Route path="/faqpage" component={faq} />
+            <Route path="/" component={Events} />
 
-              <Route path='/'>
-                <Redirect to='/' />
-              </Route>
-            </Switch>
-          </UserContext.Provider>
+            <Route path="/">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </UserContext.Provider>
       </BrowserRouter>
     </div>
   );
--- client/src/components/NonUserNavBar.js
+++ client/src/components/NonUserNavBar.js
@@ -1,62 +1,72 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "./context/userContext.js";
 
-import { 
-    Grid,
-    Button
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    navLink: {
-        textTransform: 'none',
-    }
+  navLink: {
+    textTransform: "none",
+  },
 });
 
 export default function NonUserNavBar() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const { user, setUser } = useContext(UserContext);
-    const history = useHistory();
+  const { user, setUser } = useContext(UserContext);
+  const history = useHistory();
 
-    const register = () => history.push("/register");
-    const login = () => history.push("/login");
-    const event = () => history.push("/events");
-    const contact = () => history.push("/contact");
-    const logout = () => {
-        setUser({
-            token: undefined,
-            user: undefined
-        });
-    };
+  const register = () => history.push("/register");
+  const login = () => history.push("/login");
+  const event = () => history.push("/events");
+  const contact = () => history.push("/contact");
+  const faqpage = () => history.push("/faqpage");
+  const logout = () => {
+    setUser({
+      token: undefined,
+      user: undefined,
+    });
+  };
 
-    return (
-        <Grid container className={classes.root} justify={'flex-end'}>
-            <Grid item key={"Events"}>
-                <Button onClick={event} className={classes.navLink}>Events</Button>
-            </Grid>
-            <Grid item key={"Contact"}>
-                <Button onClick={contact} className={classes.navLink}>Contact</Button>
-            </Grid>
-            
-            { user.user ? 
-                (
-                    <Grid item key={"Log Out"}>
-                        <Button className={classes.navLink} onClick={logout}>Log out</Button>
-                    </Grid>
-                ) : (
-                     <>
-                        <Grid item key={"Log In"}>
-                            <Button onClick={login} className={classes.navLink}>Log In</Button>
-                        </Grid>
-                        <Grid item key={"Log In"}>
-                            <Button onClick={register} className={classes.navLink}>Register</Button>
-                        </Grid>
-                     </>
-                )
+  return (
+    <Grid container className={classes.root} justify={"flex-end"}>
+      <Grid item key={"Events"}>
+        <Button onClick={event} className={classes.navLink}>
+          Events
+        </Button>
+      </Grid>
+      <Grid item key={"Contact"}>
+        <Button onClick={contact} className={classes.navLink}>
+          Contact
+        </Button>
+      </Grid>
+      <Grid item key={"Faqpage"}>
+        <Button onClick={faqpage} className={classes.navLink}>
+          FAQ
+        </Button>
+      </Grid>
 
-            }
+      {user.user ? (
+        <Grid item key={"Log Out"}>
+          <Button className={classes.navLink} onClick={logout}>
+            Log out
+          </Button>
         </Grid>
-    );
+      ) : (
+        <>
+          <Grid item key={"Log In"}>
+            <Button onClick={login} className={classes.navLink}>
+              Log In
+            </Button>
+          </Grid>
+          <Grid item key={"Log In"}>
+            <Button onClick={register} className={classes.navLink}>
+              Register
+            </Button>
+          </Grid>
+        </>
+      )}
+    </Grid>
+  );
 }
