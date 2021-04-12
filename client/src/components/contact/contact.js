a40ab87970608f654e25c8b06da69acd93e1f966
angelaxie2015 <angelaxie2015@gmail.com>
2021-04-12 18:10:29 -0400

    edit contact

--- client/src/components/contact/contact.js
+++ client/src/components/contact/contact.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Form, Field } from 'react-final-form';
-import { TextField } from 'final-form-material-ui';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Form, Field } from "react-final-form";
+import { TextField } from "final-form-material-ui";
 import {
   Typography,
   Paper,
   Grid,
   Button,
   InputAdornment,
-  Container
-} from '@material-ui/core';
-import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
-import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+  Container,
+} from "@material-ui/core";
+import EmailOutlinedIcon from "@material-ui/icons/EmailOutlined";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
 import { useForm, ValidationError } from "@formspree/react";
-
 
 const onSubmit = async (values) => {
   // useForm("xjvjvzyz");
@@ -78,83 +77,89 @@ function MyForm() {
     );
   }
   return (
-    <Grid container xs={12} alignContent="center" justify="center" style= {{width: "50vh", margin: "auto"}}>
-    <Paper elevation={3} style={{padding: 20}}>
-      <Typography variant="h4" align="center" component="h1" gutterBottom>
-        Contact an ambassador
-      </Typography>
-      <Typography paragraph align="center">
-        Fill out the following fields to get in contact with an ambassador.
-      </Typography>
-      <Form
-        onSubmit={onSubmit}
-        validate={validate}
-        reset={reset}
-        render={({ handleSubmit, reset, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit} noValidate>
-            <Field
-                    fullWidth
-                    required
-                    variant="filled"
-                    name="firstName"
-                    component={TextField}
-                    type="text"
-                    label="First Name"
-                    style={{ marginBottom: 8}}
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                        <PersonOutlineOutlinedIcon />
-                        </InputAdornment>
-                      ),
-                      }}
-                  />
+    <Grid
+      container
+      xs={12}
+      alignContent="center"
+      justify="center"
+      style={{ width: "50vh", margin: "auto" }}
+    >
+      <Paper elevation={3} style={{ padding: 20 }}>
+        <Typography variant="h4" align="center" component="h1" gutterBottom>
+          Contact an ambassador
+        </Typography>
+        <Typography paragraph align="center">
+          Fill out the following fields to get in contact with an ambassador.
+        </Typography>
+        <Form
+          onSubmit={onSubmit}
+          validate={validate}
+          reset={reset}
+          render={({ handleSubmit, reset, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit} noValidate>
               <Field
-                    fullWidth
-                    required
-                    variant="filled"
-                    name="lastName"
-                    component={TextField}
-                    type="text"
-                    label="Last Name"
-                    style={{ marginBottom: 8 }}
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                        <PersonOutlineOutlinedIcon />
-                        </InputAdornment>
-                      ),
-                    }}
+                fullWidth
+                required
+                variant="filled"
+                name="firstName"
+                component={TextField}
+                type="text"
+                label="First Name"
+                style={{ marginBottom: 8 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonOutlineOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
               />
               <Field
-                    name="email"
-                    fullWidth
-                    required
-                    variant="filled"
-                    component={TextField}
-                    type="email"
-                    label="Email"
-                    style={{ marginBottom: 8 }}
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                        <EmailOutlinedIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                />
-                <Field
-                    fullWidth
-                    name="feedback"
-                    required
-                    variant="filled"
-                    style={{ marginBottom: 8 }}
-                    component={TextField}
-                    multiline
-                    label="What can we help you with?"
-                />    
-               
-               <Grid container item xs={12} spacing={2}>
+                fullWidth
+                required
+                variant="filled"
+                name="lastName"
+                component={TextField}
+                type="text"
+                label="Last Name"
+                style={{ marginBottom: 8 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <PersonOutlineOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Field
+                name="email"
+                fullWidth
+                required
+                variant="filled"
+                component={TextField}
+                type="email"
+                label="Email"
+                style={{ marginBottom: 8 }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <EmailOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <Field
+                fullWidth
+                name="feedback"
+                required
+                variant="filled"
+                style={{ marginBottom: 8 }}
+                component={TextField}
+                multiline
+                label="What can we help you with?"
+              />
+
+              <Grid container item xs={12} spacing={2}>
                 <Grid item style={{ marginTop: 16 }}>
                   <Button
                     type="button"
@@ -166,7 +171,9 @@ function MyForm() {
                   </Button>
                 </Grid>
                 <Grid item style={{ marginTop: 16 }}>
-                  <Button item style={{backgroundColor: '#62d98a', color: 'black'}}
+                  <Button
+                    item
+                    style={{ backgroundColor: "#62d98a", color: "black" }}
                     variant="contained"
                     color="primary"
                     type="submit"
@@ -175,12 +182,11 @@ function MyForm() {
                     Submit
                   </Button>
                 </Grid>
-                </Grid>
-            <pre>{JSON.stringify(values, 0, 2)}</pre>
-          </form>
-        )}
-      />
-    </Paper>
+              </Grid>
+            </form>
+          )}
+        />
+      </Paper>
     </Grid>
   );
 }
