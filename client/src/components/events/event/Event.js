bb56c3133246932ca212a8dc1b737616053d1c9a
angelaxie2015 <angelaxie2015@gmail.com>
2021-04-11 22:46:14 -0400

    click on anywhere of the card to open the fulleventpage

--- client/src/components/events/event/Event.js
+++ client/src/components/events/event/Event.js
@@ -1,71 +1,70 @@
-import { React, useState, useEffect } from 'react';
-import placeholder from '../../../placeholder.png'
-import { Card, CardContent, Typography, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { React, useState, useEffect } from "react";
+import placeholder from "../../../placeholder.png";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
-import Axios from "axios"
+import Axios from "axios";
 
 const useStyles = makeStyles({
-    root: {
-      width: 350,
-      height: "100%"
-    },
-    img: {
-        width: 350,
-        height: 300, 
-        objectFit: "cover",
-    },
+  root: {
+    width: 350,
+    height: "100%",
+  },
+  img: {
+    width: 350,
+    height: 300,
+    objectFit: "cover",
+  },
 });
 
 export default function Event(props) {
-    const { event } = props;
-    const [ image, setImage ] = useState(placeholder);
+  const { event } = props;
+  const [image, setImage] = useState(placeholder);
 
-    useEffect(() => {    
-        if (event.filename) {
-            Axios.get(`http://localhost:8001/uploads/${event.filename}`)
-                .then(res => {
-                    setImage(res.config.url);
-                });
+  useEffect(() => {
+    if (event.filename) {
+      Axios.get(`http://localhost:8001/uploads/${event.filename}`).then(
+        (res) => {
+          setImage(res.config.url);
         }
-    }, []);
+      );
+    }
+  }, []);
 
-    const classes = useStyles();
-    
-    const history = useHistory();
-    const openEventDetails = (event) => {
-        history.push({
-            pathname: "/eventDetail",
-            state: event
-        });
-    };
+  const classes = useStyles();
 
-    return (
-        <Card variant="outlined"className={classes.root}>
-            <img src={image} className={classes.img}/>
-            <CardContent>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                    
-                    <a onClick={ () => openEventDetails(event) }>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {event.title}
-                        </Typography>
-                    </a>
-                    
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Typography align="right" variant="body1" component="p">
-                            {(new Date(event.date)).toDateString()}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Typography variant="body2" component="p">
-                            {event.description}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
-    );
-};
+  const history = useHistory();
+  const openEventDetails = (event) => {
+    history.push({
+      pathname: "/eventDetail",
+      state: event,
+    });
+  };
+
+  return (
+    <Card variant="outlined" className={classes.root}>
+      <a onClick={() => openEventDetails(event)}>
+        <img src={image} className={classes.img} />
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {event.title}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography align="right" variant="body1" component="p">
+                {new Date(event.date).toDateString()}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="body2" component="p">
+                {event.description}
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </a>
+    </Card>
+  );
+}
