746f9f7b302c0a4c17b389268b33a559fb3a6e76
angelaxie2015 <angelaxie2015@gmail.com>
2021-04-12 18:07:46 -0400

    display image

--- client/src/components/events/event/Event.js
+++ client/src/components/events/event/Event.js
@@ -1,70 +1,78 @@
-import { React, useState, useEffect } from 'react';
-import placeholder from '../../../placeholder.png'
-import { Card, CardContent, Typography, Grid, CardActionArea } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { React, useState, useEffect } from "react";
+import placeholder from "../../../placeholder.png";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  CardActionArea,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
 import Axios from "axios";
 
 const useStyles = makeStyles({
-    root: {
-      width: "700px",
-      height: "50vh",
-      backgroundColor: "white"
-    },
-    img: {
-        width: "100%",
-        height: "70%",  
-        objectFit: "cover",
-    },
+  root: {
+    width: "700px",
+    height: "50vh",
+    backgroundColor: "white",
+  },
+  img: {
+    width: "100%",
+    height: "70%",
+    objectFit: "cover",
+  },
 });
 
 export default function Event(props) {
   const { event } = props;
   const [image, setImage] = useState(placeholder);
 
-useEffect(() => {
-  if (event.filename) {
-    Axios.get(`http://localhost:8001/uploads/${event.filename}`).then(
-      (res) => {
-        setImage(res.config.url);
-      }
-    );
-  }
-}, []);
+  useEffect(() => {
+    if (event.filename) {
+      Axios.get(`http://localhost:8001/uploads/${event.filename}`).then(
+        (res) => {
+          setImage(res.config.url);
+        }
+      );
+    }
+  }, []);
 
-const classes = useStyles();
+  const classes = useStyles();
 
-const history = useHistory();
-const openEventDetails = (event) => {
-  history.push({
-    pathname: "/eventDetail",
-    state: event,
-  });
-};    
+  const history = useHistory();
+  const openEventDetails = (event) => {
+    history.push({
+      pathname: "/eventDetail",
+      state: event,
+    });
+  };
 
   return (
-        <CardActionArea onClick={ () => openEventDetails(event) } className={classes.root}>
-            <img src={placeholder} className={classes.img}/>
-            <CardContent style={{ height: "30%" }}>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            {event.title}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Typography align="right" variant="body1" component="p">
-                          {new Date(event.date).toDateString()}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Typography variant="body2" component="p">
-                          {event.description}
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </CardActionArea>
-
-    );
-};
+    <CardActionArea
+      onClick={() => openEventDetails(event)}
+      className={classes.root}
+    >
+      <img src={image} className={classes.img} />
+      <CardContent style={{ height: "30%" }}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Typography gutterBottom variant="h5" component="h2">
+              {event.title}
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography align="right" variant="body1" component="p">
+              {new Date(event.date).toDateString()}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body2" component="p">
+              {event.description}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </CardActionArea>
+  );
+}
