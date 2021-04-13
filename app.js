9616f697647d5f36a7284daac0b0c65f7db5f3e8
angelaxie2015 <angelaxie2015@gmail.com>
2021-04-13 10:00:43 -0400

    deployment prep

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
-import eventRouter from "./eventRouter.js"
-import userRouter from "./userRouter.js"
-import gridfsRouter from "./gridfsRouter.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import eventRouter from "./eventRouter.js";
+import userRouter from "./userRouter.js";
+import gridfsRouter from "./gridfsRouter.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 8001;
@@ -14,7 +15,15 @@ app.use(cors());
 
 app.get("/", (req, res) => res.status(200).send("hello world"));
 
-app.listen(PORT, () => console.log(`The server started on port: ${ PORT }`));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`The server started on port: ${PORT}`));
 
 app.use("/events", eventRouter);
 app.use("/users", userRouter);
