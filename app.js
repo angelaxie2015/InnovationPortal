7c180b6a2ce53bf8a458dbfa1bca5b2af52b21d7
angelaxie2015 <angelaxie2015@gmail.com>
2021-04-16 16:34:51 -0400

    change backend

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import gridfsRouter from "./gridfsRouter.js";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 8001;
+
 dotenv.config();
 
 app.use(express.json());
@@ -17,8 +17,6 @@ app.use("/events", eventRouter);
 app.use("/users", userRouter);
 app.use("/uploads", gridfsRouter);
 
-app.get("/", (req, res) => res.status(200).send("hello world"));
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
@@ -27,4 +25,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+app.use("/", (req, res) => res.status(200).send("hello world"));
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => console.log(`The server started on port: ${PORT}`));
